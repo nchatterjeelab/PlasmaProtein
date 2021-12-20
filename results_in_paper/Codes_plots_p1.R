c435944bd78c2b0e55597c7c13798ec1524c66fb
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2021-12-19 19:06:19 -0500

    Update Codes_plots_p1.R
    reformat figures

--- results_in_paper/Codes_plots_p1.R
+++ results_in_paper/Codes_plots_p1.R
@@ -1,25 +1,37 @@
 library(ggplot2)
 
+My_Theme = theme(
+  panel.background = element_blank(), 
+  title = element_text(size = 8),
+  text = element_text(size = 7)
+  # axis.title.x = element_text(size = 10),
+  # axis.text.x = element_text(size = 8),
+  # axis.title.y = element_text(size = 10),
+  # axis.text.y = element_text(size = 8),
+  # legend.title = element_text(size = 10)
+  # legend.text = element_text(size = 8)
+)
+
 ## Fig 1
 
 #### A
 
 library(readxl)
 
-tmp <- read_excel("/Users/jnz/Dropbox/PWAS_manuscript/NatureGenetics/2021_11_revision4/Suppl_tables.xlsx", sheet = "ST2 -- PEER factors", skip=3)
-tmp <- tmp[1:21,]
+tmp <- read_excel("/Users/jnz/Dropbox/PWAS_manuscript/NatureGenetics/2021_12_revision4/Suppl_tables.xlsx", sheet = "ST2 -- PEER factors", skip=3)
+tmp <- tmp[1:20,]
 df.peer <- data.frame(NPEER=rep(tmp[[1]],2), NpGene=c(tmp[[2]], tmp[[3]]), Race=c(rep("EA", nrow(tmp)), rep("AA", nrow(tmp))))
 df.peer$NPEER <- as.integer(as.character(df.peer$NPEER))
 df.peer <- df.peer[df.peer$NPEER<140,]
 df.peer_highlight <- df.peer[as.character(c(10,30)),]
 
 p1 <- ggplot(data = df.peer, aes(x = NPEER, y=NpGene, col=Race)) + 
-    geom_line() + geom_point() +
-    geom_point(data=df.peer_highlight,aes(x = NPEER, y=NpGene, color=Race, fill=Race), size=5,shape=18) +
-    theme(panel.background = element_blank()) +
-    labs(y = "# significant SOMAmers ", x="No. of PEER factors", title= NULL) +
-    scale_colour_manual(values=c("#238b45","#2171b5")) + 
-    scale_fill_manual(values=c("#238b45","#2171b5"))
+  geom_line() + geom_point() +
+  geom_point(data=df.peer_highlight,aes(x = NPEER, y=NpGene, color=Race, fill=Race), size=5,shape=18) +
+  My_Theme +
+  labs(y = "# significant SOMAmers      ", x="No. of PEER factors", title= NULL) +
+  scale_colour_manual(values=c("#238b45","#2171b5")) + 
+  scale_fill_manual(values=c("#238b45","#2171b5"))
 
 ##### B
 library(ggVennDiagram)
@@ -33,16 +45,16 @@ df.venn <- data.frame(x = c(-0.1,0.1),
                       r = c(0.4,0.45),
                       labels = c('A', 'B'))
 p2 <-  ggplot(df.venn, aes(x0 = x, y0 = y, r = r, fill = labels)) +
-    geom_circle(alpha = .4, size = 1, colour = NA) +
-        theme_void() +
-    theme(legend.position="none",
-          panel.background = element_blank()) +
-    annotate("text", x = -0.4, y =0.4, label = "1,618 in AA")+
-    annotate("text", x = 0, y = 0, label = "1,447 overlapping")+
-    annotate("text", x = 0.4, y =0.43, label = "2,004 in EA")+
-    coord_fixed()+
-    scale_fill_manual(values=c("#238b45","#2171b5"))
-    
+  geom_circle(alpha = .4, size = 1, colour = NA) +
+  theme_void() +
+  theme(legend.position="none")+
+  My_Theme+
+  annotate("text", x = -0.32, y =0.4, label = "1,618 in AA", size=3)+
+  annotate("text", x = 0, y = 0, label = "1,447 overlapping", size=3)+
+  annotate("text", x = 0.4, y =0.43, label = "2,004 in EA", size=3)+
+  coord_fixed()+
+  scale_fill_manual(values=c("#238b45","#2171b5"))
+
 ## C
 
 sig.w <- read_tsv("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Tables/1_pQTL_summary_cleaned_4.0_EA.txt")
@@ -72,11 +84,11 @@ df.effmaf <- data.frame(BETA = c(sig.w$Beta, sig.b$Beta),
 p3 <- ggplot(data = df.effmaf, aes(x = maf, y = abs(BETA), col=Race)) +
   geom_point(alpha=0.5,size=0.5) +
   # geom_point(alpha=0.5,size=power) +  
-  geom_smooth(method="lm", mapping = aes(weight = 1/power), col="#cb181d") +
-  geom_smooth(method="lm", col="black") +
+  geom_smooth(method="lm", mapping = aes(weight = 1/power), col="#fd8d3c") +
+  geom_smooth(method="lm", col="#525252") +
   scale_x_continuous(breaks=c(0, 0.1, 0.2)) +
-  theme(legend.position="none",
-        panel.background = element_blank()) +
+  theme(legend.position="none")+
+  My_Theme+
   facet_wrap(~Race,  ncol=2)+
   labs(x="MAF(1-MAF)", y = "Effect size", title = NULL)+
   scale_colour_manual(values=c("#238b45","#2171b5"))
@@ -90,10 +102,9 @@ df.efftss <- data.frame(BETA = c(sig.w$Beta, sig.b$Beta),
                                  rep("AA", nrow(sig.b))))
 p4 <- ggplot(data = df.efftss, aes(x = dist, y = abs(BETA), col=Race)) + 
   geom_point(alpha=0.5,size=0.5) +  
-  theme(legend.position="none") +
   scale_x_continuous(breaks=c(-0.25*10^6,0.25*10^6)) +
-  theme(legend.position="none",
-        panel.background = element_blank()) +
+  theme(legend.position="none")+
+  My_Theme+
   facet_wrap(~Race,  ncol=2)+
   labs(x="Distance to TSS", y = "Effect size", title = NULL)+
   scale_colour_manual(values=c("#238b45","#2171b5"))+
@@ -112,8 +123,8 @@ df.cond <- data.frame(count=c(data.frame(sort(table(sig.w$SOMAmer),decreasing=T)
                              rep("AA", length(data.frame(sort(table(sig.b$SOMAmer),decreasing=T))$Freq))))
 p5 <- ggplot(df.cond, aes(x = count)) + 
   geom_histogram(binwidth = 1, aes(fill=Race), col="white",alpha=0.8) +
-  theme(legend.position="none",
-        panel.background = element_blank()) +
+  theme(legend.position="none")+
+  My_Theme+
   labs(x = "# conditionally significant cis-SNPs", 
        y = "# significant SOMAmers",title=NULL) + 
   facet_grid(cols = vars(Race))+
@@ -132,9 +143,9 @@ p <- ggarrange(ggarrange(p1, p2,
                          widths = c(0.35,0.35,0.3)),
                nrow = 2, heights = c(0.4,0.6))
 
-ggsave(filename="p1.png", 
-       plot=p, device="png",
+ggsave(filename="p1.pdf", 
+       plot=p, device="pdf",
        path="/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Figures/", 
-       width=12, height=7.5, units="in", dpi=500)
+       width=200, height=115, units="mm", dpi=320)
 
 
