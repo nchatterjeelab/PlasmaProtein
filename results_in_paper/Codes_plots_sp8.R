e277e8b9e40a5a1790a83feea0aeca21c416f17d
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2021-12-21 22:49:58 -0500

    Reformat figures
    Reformat figures

--- results_in_paper/Codes_plots_sp8.R
+++ results_in_paper/Codes_plots_sp8.R
@@ -1,15 +1,35 @@
+
+## Suppl Fig 8
+
 library(readr)
 library(gaston)
 
+disease <- "Urate"
+dat <- read_tsv(paste0("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*RData/PWAS/PWAS_",disease,"_CI.txt"))
+dat1 <- dat[!is.na(dat$PWAS.Z),]
+
 disease <- "Gout"
-dat <- read_tsv(paste0("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*RData/PWAS_",disease,"_CI.txt"))
-dat <- dat[!is.na(dat$PWAS.Z),]
+dat <- read_tsv(paste0("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*RData/PWAS/PWAS_",disease,"_CI.txt"))
+dat2 <- dat[!is.na(dat$PWAS.Z),]
 
 
-ggsave(filename=paste0("sp9_",disease,".png"), 
-       plot=qqplot.pvalues(dat$PWAS.P, col.CB ="#E41A1C32", col.abline = "red", col= alpha("#4292c6", 0.6), pch=20, bty="n",
-                           main=paste0("QQ plot of PWAS p-values of ",disease)), 
-       device="png",
-       path="/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Figures/sp/", 
-       width=5, height=5, units="in", dpi=500)
+pdf("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Figures/sp/sp8.pdf", width = 7.87, height = 3.94)
+par(mfrow=c(1,2))
+qqplot.pvalues(dat1$PWAS.P, col.CB ="#E41A1C32", col.abline = "red", col= alpha("#4292c6", 0.6), 
+               pch=20, bty="n",
+               font.main=1,
+               cex.axis = .75,
+               cex.lab = .9,
+               cex.main = 1.1,
+               main=paste0("QQ plot of Urate PWAS p-values"))
+
+qqplot.pvalues(dat2$PWAS.P, col.CB ="#E41A1C32", col.abline = "red", col= alpha("#4292c6", 0.6), 
+               pch=20, bty="n",
+               font.main=1,
+               cex.axis = .75,
+               cex.lab = .9,
+               cex.main = 1.1,
+               main=paste0("QQ plot of Gout PWAS p-values"))
+dev.off()
+
 
