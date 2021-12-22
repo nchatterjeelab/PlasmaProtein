cdd979263dc96ad271a13a267c1732aaf6da2094
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2021-12-21 22:49:40 -0500

    Reformat figures
    Reformat figures

--- results_in_paper/Codes_plots_sp7.R
+++ results_in_paper/Codes_plots_sp7.R
@@ -1,13 +1,20 @@
+
+## Suppl Fig 7
+
 library(readr)
 library(gaston)
 
-dat <- read_tsv(paste0("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*RData/PWAS_null_cont_normal.txt"))
+dat <- read_tsv(paste0("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*RData/PWAS/PWAS_null_cont_normal.txt"))
 dat <- dat[!is.na(dat$PWAS.Z),]
 
-ggsave(filename=paste0("sp7.png"), 
-       plot=qqplot.pvalues(dat$PWAS.P, col.CB ="#E41A1C32", col.abline = "red", col= alpha("#4292c6", 0.4), pch=20, bty="n",
-                           main=paste0("QQ plot of PWAS p-values\nof null phenotype (continuous)")), 
-       device="png",
-       path="/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Figures/sp/", 
-       width=5, height=5, units="in", dpi=500)
+pdf("/Users/jnz/Document/JHU/Research/PWAS/Analysis/500Kb/*Figures/sp/sp7.pdf", width = 3.94, height = 3.94)
+qqplot.pvalues(dat$PWAS.P, col.CB ="#E41A1C32", col.abline = "red", col= alpha("#4292c6", 0.4), 
+               pch=20, bty="n",
+               font.main=1,
+               cex.axis = .65,
+               cex.lab = .8,
+               cex.main = 0.95,
+               main=paste0("QQ plot of null PWAS p-values")
+)
+dev.off()
 
