7118a547f7364e2b4350c43147251766c7b03507
Diptavo Dutta <dutta.diptavo@gmail.com>
2022-01-02 11:53:12 -0500

    enr

---
+++ pQTL/9_enrichment/enrichment_torus.sh
@@ -0,0 +1,34 @@
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_enhancer_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_promoter_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_open_chromatin_region_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_promoter_flanking_region_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_CTCF_binding_site_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_TF_binding_site_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_3_prime_UTR_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_5_prime_UTR_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_frameshift_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_intron_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_missense_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_non_coding_transcript_exon_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_splice_acceptor_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_splice_donor_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_splice_region_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_stop_gained_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
+/dcl01/chatterj/data/diptavo/torus -d /dcl01/chatterj/data/diptavo/pQTL/enr/cispqtl.ARIC.gz  -smap /dcl01/chatterj/data/diptavo/pQTL/enr/snp_ARIC.info.map.gz -gmap /dcl01/chatterj/data/diptavo/pQTL/enr/soma_ARIC.info.gz -annot /dcl01/chatterj/data/diptavo/pQTL/enr/snp.annotation_synonymous_variant_d.gz  -est > /dcl01/chatterj/data/diptavo/pQTL/enr/synonymous.enrichment.est
+
