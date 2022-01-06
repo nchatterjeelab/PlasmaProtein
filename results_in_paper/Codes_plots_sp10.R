d5ba098b36ad125aff9128cd9e27c322b2f8a655
Diptavo Dutta <dutta.diptavo@gmail.com>
2022-01-06 11:06:46 -0500

    sp10

---
+++ results_in_paper/Codes_plots_sp10.R
@@ -0,0 +1,7 @@
+pc2 <- read.table("ExtendedDataFig10.txt",heade = T)
+
+pc.pr <- ggpairs(pc2[,3:7],aes(color = pc2$V1),upper = list(continuous = "points"),diag = list(continuous = "blank")) + 
+  scale_color_manual(values=c("#238b45","#2171b5")) + 
+  theme(panel.background = element_blank(),legend.position = "bottom",axis.text = element_text(size = 6),axis.title = element_text(size = 7)) + 
+  scale_x_continuous(breaks = c(-0.01,0,0.01),labels = c(-0.01,0,0.01)) + 
+  scale_y_continuous(breaks = c(-0.01,0,0.01),labels = c(-0.01,0,0.01))
