4dc2072430ae6ea13cfa1fbcb39ec0243fcbe0b9
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2022-01-06 09:17:23 -0500

    Update Codes_plots_sp2.R

--- results_in_paper/Codes_plots_sp2.R
+++ results_in_paper/Codes_plots_sp2.R
@@ -1,27 +1,87 @@
-eqtls <- read.table("~/ExtendedDataFig2.csv",header = T)
-eqtls.2 <- read.csv("~/ExtendedDataFig2_leg.csv",header = T)
 
+## Suppl Fig 2
+
+library(latex2exp)
+library(ggplot2)
+
+
+eqtls = read_excel("ExtendedDataFig2.xlsx", sheet = "dat")
+eqtls.2 = read_excel("ExtendedDataFig2.xlsx", sheet = "leg")
 
 
 im1 <- ggplot(eqtls, aes(x = 1:49,y=V2, size=sample)) +
-  geom_point(alpha=1,color = eqtls$cls)+  theme(plot.title = element_text(hjust = 0.5,size = 7),axis.title.x = element_text(size = 6),axis.title.y = element_text(size = 6),
-                                                panel.background = element_blank(),axis.text.x = element_blank(),axis.line = element_line(color = "black",size = 0.5),legend.text = element_text(size = 6),
-                                                legend.title = element_text(size = 6),axis.text = element_text(size = 6))+
+  geom_point(alpha=1,color = eqtls$cls)+  
+  theme(plot.title = element_text(hjust = 0.5,size = 7),
+        axis.title.x = element_text(size = 6),
+        axis.title.y = element_text(size = 6),
+        panel.background = element_blank(),
+        axis.text.x = element_blank(),
+        axis.line = element_line(color = "black",size = 0.5),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 6),
+        axis.text = element_text(size = 6)) +
   labs(title = "Overlap with eQTLs (GTEx V8)", x="Tissues",y="Proportion")+
   scale_x_continuous(breaks = NULL)+
   coord_cartesian(ylim = c(0,0.5)) + scale_fill_manual(values = as.character(eqtls$cls))
 
 
 im2 <- ggplot(eqtls, aes(x = 1:49,y=V3, size=sample)) +
-  geom_point(alpha=1,color = eqtls$cls)+  theme(plot.title = element_text(hjust = 0.5,size = 7),axis.title.x = element_text(size = 6),axis.title.y = element_text(size = 6),
-                                                panel.background = element_blank(),axis.text.x = element_blank(),axis.line = element_line(color = "black",size = 0.5),legend.text = element_text(size = 6),
-                                                legend.title = element_text(size = 6),axis.text = element_text(size = 6))+
+  geom_point(alpha=1,color = eqtls$cls)+ 
+  theme(plot.title = element_text(hjust = 0.5,size = 7),
+        axis.title.x = element_text(size = 6),
+        axis.title.y = element_text(size = 6),
+        panel.background = element_blank(),
+        axis.text.x = element_blank(),
+        axis.line = element_line(color = "black",size = 0.5),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 6),
+        axis.text = element_text(size = 6)) +
   labs(title = "Colocalization with eQTLs (GTEx V8)", x="Tissues",y="Proportion")+
   scale_x_continuous(breaks = NULL)+
-  coord_cartesian(ylim = c(0,0.25)) + scale_fill_manual(values = as.character(eqtls$cls))
+  coord_cartesian(ylim = c(0,0.25)) + 
+  scale_fill_manual(values = as.character(eqtls$cls))
 
-im3 <- ggplot(eqtls.2,aes(x = 1:49,y = V3)) + geom_point(aes(color = as.character(eqtls.2$cls))) +
-  scale_color_manual(drop = FALSE,name = "GTEx V8 tissues", values = as.character(eqtls.2$cls),labels = eqtls$tissues) + theme(legend.background = element_blank(),legend.text = element_text(size = 3),legend.key = element_blank(),legend.title = element_text(size = 7))
+# eqtls.2$tissues <- as.factor(eqtls.2$tissues)
+# eqtls$tissues <- as.factor(eqtls$tissues)
+myColors <- eqtls$cls
+names(myColors) <- eqtls$tissues
+
+im3 <- ggplot(eqtls.2, aes(x = 1:49,y = V3)) + 
+  geom_point(aes(color = tissues)) +
+  scale_color_manual(name = "GTEx V8 tissues",
+                     values = myColors) +
+  theme(
+    legend.key = element_blank(),
+    legend.key.size = unit(2, "mm"),
+    panel.border = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    title = element_text(size = 7),
+    text = element_text(size = 6)
+  ) +
+  guides(color=guide_legend(ncol = 1))
+
 pm3 <- as_ggplot(get_legend(im3))
+
+###############################################################
+###############################################################
+###############################################################
+
+
+p <- ggarrange(ggarrange(im1, im2,
+                         nrow = 2, labels = c("a", "b"),
+                         heights = c(0.5,0.5)),
+               pm3,
+               ncol = 2, 
+               labels = c(NA, NA),
+               widths = c(0.7,0.3)
+)
+
+ggsave(filename=paste0("ExtendedDataFigure2.pdf"), 
+       plot=p, device="pdf",
+       path="/Users/jnz/Dropbox/PWAS_manuscript/NatureGenetics/2021_12_revision4/Final_files_prepared_for_submission/Figures/", 
+       width=180, height=120, units="mm", dpi=320)
+
 
 
