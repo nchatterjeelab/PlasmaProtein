ec8c95a9c476b7d5448f1bc35f0f6f083757d43c
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2022-01-06 14:28:05 -0500

    Update Codes_plots_sp10.R

--- results_in_paper/Codes_plots_sp10.R
+++ results_in_paper/Codes_plots_sp10.R
@@ -1,4 +1,10 @@
+
+## Suppl Fig 8
+
+library(gaston)
+library(ggplot2)
 library(GGally)
+
 
 pc2 <- read.table("ExtendedDataFig10.txt",heade = T)
 
@@ -7,3 +13,11 @@ pc.pr <- ggpairs(pc2[,3:7],aes(color = pc2$V1),upper = list(continuous = "points
   theme(panel.background = element_blank(),legend.position = "bottom",axis.text = element_text(size = 6),axis.title = element_text(size = 7)) + 
   scale_x_continuous(breaks = c(-0.01,0,0.01),labels = c(-0.01,0,0.01)) + 
   scale_y_continuous(breaks = c(-0.01,0,0.01),labels = c(-0.01,0,0.01))
+
+ggsave(filename=paste0("ExtendedDataFigure10.pdf"), 
+     plot=pc.pr, device="pdf",
+     path="/Users/jnz/Dropbox/PWAS_manuscript/NatureGenetics/2021_12_revision4/Final_files_prepared_for_submission/Figures/", 
+     width=180, height=180, units="mm", dpi=320)
+
+
+
