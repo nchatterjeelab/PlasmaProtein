e6e5fc48850ab71f8c45c4477a7204a54340d4da
Jingning Zhang <55093876+Jingning-Zhang@users.noreply.github.com>
2022-01-06 09:18:28 -0500

    Update Codes_plots_sp4.R

--- results_in_paper/Codes_plots_sp4.R
+++ results_in_paper/Codes_plots_sp4.R
@@ -1,7 +1,13 @@
+
+## Suppl Fig 4
+
+library(latex2exp)
+library(ggplot2)
+
 barCOLS = c("#008fd5","#de6b35")
 dotCOLS = c("#a6d8f0","#f9b282")
 
-ax1 = read.csv("ExtendedDataFig4a.csv")
+ax1 = read_excel("ExtendedDataFig4.xlsx", sheet = "4a")
 enr1 <- ggplot(ax1, aes(x=X, y=odr.ea, ymin=log(lower.ci,base = 2), ymax=log(upper.ci,base = 2))) +
   #specify position here
   geom_linerange(size=1,position=position_dodge(width = 0.2)) +
@@ -18,7 +24,7 @@ enr1 <- ggplot(ax1, aes(x=X, y=odr.ea, ymin=log(lower.ci,base = 2), ymax=log(upp
   ggtitle(paste0("European Americans (EA)"))
 
 
-ax2 = read.csv("ExtendedDataFig4b.csv")
+ax2 = read_excel("ExtendedDataFig4.xlsx", sheet = "4b")
 enr2 <- ggplot(ax2, aes(x=X, y=odr.aa, ymin=log(lower.ci,base = 2), ymax=log(upper.ci,base = 2))) +
   #specify position here
   geom_linerange(size=1,position=position_dodge(width = 0.2)) +
@@ -28,15 +34,15 @@ enr2 <- ggplot(ax2, aes(x=X, y=odr.aa, ymin=log(lower.ci,base = 2), ymax=log(upp
   scale_fill_manual(values=barCOLS)+
   scale_color_manual(values=dotCOLS)+
   scale_x_discrete(name="Functional Annotations") +
-  scale_y_continuous(name=expression("log"[2]*"(enrichment)"), limits = c(-4, 8),) +
+  scale_y_continuous(name=expression("log"[2]*"(enrichment)"), limits = c(-4, 8)) +
   coord_flip() +
   theme_minimal() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
                           panel.grid.minor = element_blank(),axis.title = element_text(size = 6),title = element_text(size = 7),axis.text = element_text(size = 6)) + 
   ggtitle("African Americans (AA)")
 
 
-enr.aa.x = read.csv("ExtendedDataFig4c.csv")
-enr3 <- ggplot(enr.aa.x, aes(x=X, y=V2, ymin=sapply(V4,FUN = function(x){return(max(x,-1))}), ymax=V5)) +
+enr.aa.x = read_excel("ExtendedDataFig4.xlsx", sheet = "4c")
+enr3 <- ggplot(enr.aa.x, aes(x=X1, y=V2, ymin=sapply(V4,FUN = function(x){return(max(x,-1))}), ymax=V5)) +
   #specify position here
   geom_linerange(size=1,position=position_dodge(width = 0.2)) +
   geom_hline(yintercept=0, lty=2,colour = "grey") +
@@ -53,8 +59,8 @@ enr3 <- ggplot(enr.aa.x, aes(x=X, y=V2, ymin=sapply(V4,FUN = function(x){return(
 
 
 
-enr.ea.x = read.csv("ExtendedDataFig4d.csv")
-enr4 <- ggplot(enr.ea.x, aes(x=X, y=V2, ymin=sapply(V4,FUN = function(x){return(max(x,-1))}), ymax=V5)) +
+enr.ea.x = read_excel("ExtendedDataFig4.xlsx", sheet = "4d")
+enr4 <- ggplot(enr.ea.x, aes(x=X1, y=V2, ymin=sapply(V4,FUN = function(x){return(max(x,-1))}), ymax=V5)) +
   #specify position here
   geom_linerange(size=1,position=position_dodge(width = 0.2)) +
   geom_hline(yintercept=0, lty=2,colour = "grey") +
@@ -67,6 +73,30 @@ enr4 <- ggplot(enr.ea.x, aes(x=X, y=V2, ymin=sapply(V4,FUN = function(x){return(
   coord_flip() + ggtitle(paste0("African Americans (AA)")) +
   theme_minimal() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
                           panel.grid.minor = element_blank(),axis.title = element_text(size = 6),title = element_text(size = 7),axis.text = element_text(size = 6))
+
+
+###############################################################
+###############################################################
+###############################################################
+
+
+p <- ggarrange(ggarrange(enr1, enr2,
+                         ncol = 2, labels = c("a", "b"),
+                         heights = c(0.5,0.5)),
+               ggarrange(enr3, enr4,
+                         ncol = 2, labels = c("c", "d"),
+                         heights = c(0.5,0.5)),
+               nrow = 2, 
+               labels = c(NA, NA),
+               widths = c(0.5,0.5)
+)
+
+ggsave(filename=paste0("ExtendedDataFigure4.pdf"), 
+       plot=p, device="pdf",
+       path="/Users/jnz/Dropbox/PWAS_manuscript/NatureGenetics/2021_12_revision4/Final_files_prepared_for_submission/Figures/", 
+       width=180, height=105, units="mm", dpi=320)
+
+
 
 
 
